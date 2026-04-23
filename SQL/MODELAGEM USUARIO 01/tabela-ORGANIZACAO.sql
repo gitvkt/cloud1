5c23158d87f9fc1d707d5d9e3e794f60de94b6a0
Vkt Sistemas <vktsistemas@gmail.com>
2026-04-22 22:38:09 -0300

    Modelagem01

---
+++ SQL/MODELAGEM USUARIO 01/tabela-ORGANIZACAO.sql
@@ -0,0 +1,22 @@
+CREATE TABLE ORGANIZACAO (
+    id_organizacao_pk INT AUTO_INCREMENT PRIMARY KEY, -- chave primaria
+    uuid_organizacao CHAR(36) DEFAULT (UUID()) NOT NULL UNIQUE, -- UUID automático
+    
+    -- Chave Estrangeira
+    id_organizacao_fk INT NOT NULL,
+
+    cnpj_organizacao VARCHAR(20) NOT NULL UNIQUE,
+    nome_fantasia_organizacao VARCHAR(100) NOT NULL,
+    razao_social_organizacao VARCHAR(100) NOT NULL,
+    
+    status_organizacao ENUM("Ativa", "Inativa", "Suspensa", "Bloqueada") DEFAULT "Ativa",
+    
+    data_cadastro_organizacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    data_atualizacao_organizacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+
+    -- Regra nomeada com chave estrangeira apontada para tabela USUARIO e seu respectivo id
+    CONSTRAINT fk_id_organizacao
+    FOREIGN KEY (id_organizacao_fk)
+    REFERENCES USUARIO(id_usuario_pk)
+    ON DELETE RESTRICT
+);
